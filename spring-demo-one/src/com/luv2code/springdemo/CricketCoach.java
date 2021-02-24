e70635096b8d1d6d298c439aa659bb2767f0aa92
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-02-24 17:14:42 +0600

    Literal values and values from properties files

--- spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -4,6 +4,10 @@ public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	//add new fields for email and team
+	private String email;
+	private String team;
+	
 	//create no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constuctor");
@@ -13,6 +17,24 @@ public class CricketCoach implements Coach {
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside setter method");
 		this.fortuneService = fortuneService;
+	}	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		System.out.println("CricketCoach: inside setter method - set Email address");
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - set team name");
+		this.team = team;
 	}
 
 	@Override
--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -11,11 +11,22 @@ public class SetterDemoApp {
 		
 		//retrieve bean from spring container
 		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		CricketCoach theCoach2 = context.getBean("myCricketCoachAnother", CricketCoach.class);
 		
 		//call method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		//call our new methods to get literal values
+		System.out.println(theCoach.getEmail());
+		
+		System.out.println(theCoach.getTeam());
+		
+		//call our values from properties file
+		System.out.println(theCoach2.getEmail());
+		
+		System.out.println(theCoach2.getTeam());
 		
 		//close the context
 		context.close();
