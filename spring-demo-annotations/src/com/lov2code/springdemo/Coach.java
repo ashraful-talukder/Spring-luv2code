75aacbe185c3d92094f20f870fb67599e9af741a
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-02-27 12:47:20 +0600

    annotations using dependency injection

--- spring-demo-annotations/src/com/lov2code/springdemo/Coach.java
+++ spring-demo-annotations/src/com/lov2code/springdemo/Coach.java
@@ -3,4 +3,6 @@ package com.lov2code.springdemo;
 public interface Coach {
 
 	public String getDailyWorkout();
+	
+	public String getDailyFortune();
 }
--- spring-demo-annotations/src/com/lov2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/lov2code/springdemo/TennisCoach.java
@@ -1,5 +1,6 @@
 package com.lov2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /*if we don't use any name inside of the component then to get bean 
@@ -9,9 +10,41 @@ first letter of the class name should be in small letter*/
 @Component("thatSillyCoach")
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	//default constructor
+	public TennisCoach() {
+		System.out.println(">> inside default constructor");
+	}
+	
+	/*// constructor injection for dependency injection
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}*/
+	
+	/*// define a setter method for dependency injection
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> inside setter method");
+		fortuneService = theFortuneService;
+	}*/
+	
+	// method injection for dependency injection
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff method");
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
