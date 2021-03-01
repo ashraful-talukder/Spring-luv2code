bea879636fcc7fe53c34a888014e7f18588f657a
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-03-01 17:53:51 +0600

    @Qualifier for dependency injection

---
+++ spring-demo-annotations/src/com/lov2code/springdemo/RESTFortuneService.java
@@ -0,0 +1,13 @@
+package com.lov2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
--- spring-demo-annotations/src/com/lov2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/lov2code/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.lov2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /*if we don't use any name inside of the component then to get bean 
@@ -10,7 +11,12 @@ first letter of the class name should be in small letter*/
 @Component("thatSillyCoach")
 public class TennisCoach implements Coach {
 
+	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
+	
+	/*// we will use the following declaration in the case of method/constructor/method injection
+	private FortuneService fortuneService;*/
 	
 	//default constructor
 	public TennisCoach() {
@@ -30,12 +36,12 @@ public class TennisCoach implements Coach {
 		fortuneService = theFortuneService;
 	}*/
 	
-	// method injection for dependency injection
+	/*// method injection for dependency injection
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside doSomeCrazyStuff method");
 		fortuneService = theFortuneService;
-	}
+	}*/
 	
 	@Override
 	public String getDailyWorkout() {
