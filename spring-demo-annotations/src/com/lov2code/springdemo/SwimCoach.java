cdf1d122eb73d7528cc475c6a2ed6ed807d1e30c
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-03-02 11:06:32 +0600

    Spring Configuration with Java Code(No XML)

---
+++ spring-demo-annotations/src/com/lov2code/springdemo/SwimCoach.java
@@ -0,0 +1,21 @@
+package com.lov2code.springdemo;
+
+public class SwimCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Swim 1000 meters as a warm up.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
