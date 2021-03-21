fcfdd2f76f3f113f6da0040d0150a2797d602926
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-03-21 11:51:14 +0600

    cutome validation

--- spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -12,6 +12,8 @@
 	Free Passes: ${customer.freePasses}
 	<br> <br>
 	Postal Code: ${customer.postalCode}
+	<br> <br>
+	Course Code: ${customer.courseCode}
 
 </body>
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 
 public class Customer {
 
@@ -22,6 +24,10 @@ public class Customer {
 	
 	@Pattern(regexp="[A-Z0-9]{5}", message="5 char/digits")
 	private String postalCode;
+	
+	@CourseCode(value="ASH", 
+			message="must start with ASH")
+	private String courseCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -53,6 +59,14 @@ public class Customer {
 
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}
 
 	
