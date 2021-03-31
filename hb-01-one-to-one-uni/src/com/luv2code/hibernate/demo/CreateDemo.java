8924e1f820094f910aa7564c32e83a92cca7b11a
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-03-31 14:56:41 +0600

    hb-01-one-to-one-uni project added

---
+++ hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure()
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		
+		// create a session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create the objects
+			Instructor tempInstrutor = new Instructor("Ashraful", "Talukder", "talukder@gmail.com");
+			InstructorDetail tempInstructorDetail = new InstructorDetail("https://youtube.com/ashraful-talukder", "Photography");
+			
+			// associate the objects
+			tempInstrutor.setInstructionDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			System.out.println("Saving instructor: "  + tempInstructorDetail);
+			System.out.println("Saving instructor: "  + tempInstrutor);
+			session.save(tempInstrutor);	// this will also save the details object because of "CascadeType.ALL"
+			
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
