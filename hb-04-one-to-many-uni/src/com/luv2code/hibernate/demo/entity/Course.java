b93d02c8f384480dcda877a361d3da7f5c10e3dc
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-04-05 12:53:42 +0600

    Review added

--- hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -38,6 +43,11 @@ public class Course {
 						CascadeType.MERGE, CascadeType.REFRESH})
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
+	
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
 	
 	public Course() {
 		
@@ -72,8 +82,26 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + "]";
+	}
+	
+	// add a convenient method
+	
+	public void addReview(Review theReview) {
+		if(reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		
+		reviews.add(theReview);
 	}
 }
--- hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -54,10 +55,10 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 	
-	@OneToMany(mappedBy="instructor", cascade= {CascadeType.DETACH,
-												CascadeType.MERGE, 
-												CascadeType.PERSIST, 
-												CascadeType.REFRESH})
+	@OneToMany(fetch=FetchType.LAZY, 
+				mappedBy="instructor", 
+				cascade= {CascadeType.DETACH, CascadeType.MERGE, 
+							CascadeType.PERSIST, CascadeType.REFRESH})
 	private List<Course> courses;
 	
 	public Instructor() {
