b6d74ca350cd1b5161b1928da605e2295aa5b1cb
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-04-06 15:19:10 +0600

    log

--- hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -49,6 +49,8 @@ public class Course {
 	@JoinColumn(name="course_id")
 	private List<Review> reviews;
 	
+	private List<Student> students;
+	
 	public Course() {
 		
 	}
@@ -88,6 +90,14 @@ public class Course {
 
 	public void setReviews(List<Review> reviews) {
 		this.reviews = reviews;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
 	}
 
 	@Override
---
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Student.java
@@ -0,0 +1,10 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+}
