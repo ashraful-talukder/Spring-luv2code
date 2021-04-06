f0e40cc4febcb2ae404d67b3ad1eb1bcdaf02ab7
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-04-06 15:52:24 +0600

    recovery mode

---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -0,0 +1,117 @@
+package com.luv2code.hibernate.demo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+	/*
+	1. define our fields
+	
+	2. define constructors
+	
+	3. define getters and setters
+	
+	4. define toString
+	
+	5. annotate filed
+	*/
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.DETACH,
+						CascadeType.MERGE, CascadeType.REFRESH})
+	@JoinColumn(name="instructor_id")
+	private Instructor instructor;
+	
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
+	
+	private List<Student> students;
+	
+	public Course() {
+		
+	}
+
+	public Course(String title) {
+		super();
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + "]";
+	}
+	
+	// add a convenient method
+	
+	public void addReview(Review theReview) {
+		if(reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		
+		reviews.add(theReview);
+	}
+}
