ebba895dc89eb416adc6982a896aa8d1127528a6
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-04-15 10:50:24 +0600

    more of pointcut expressions

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -18,7 +18,8 @@ public class MainDemoApp {
 		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 		
 		// call the business method
-		theAccountDAO.addAccount();
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
 		
 		// call the membership business method
 		theMembershipDAO.addSillyMember();
--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,7 @@ public class MyDemoLoggingAspect {
 	
 	// let's start with an @Before advice
 	
-	@Before("execution(public void add*())")
+	@Before("execution(* add*(..))")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n===============>>> Executing @Before advice on addAccount()\n");
