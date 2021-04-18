b2ca8156ac2f22e7b42ede3ce4c87b107cd6a0a3
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-04-18 13:46:28 +0600

    @AfterReturn

---
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,42 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+import com.luv2code.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+	
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		
+		System.out.println(getClass() + ": Doing my DB work: Adding an account");
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork()");
+		return true;
+	}
+}
