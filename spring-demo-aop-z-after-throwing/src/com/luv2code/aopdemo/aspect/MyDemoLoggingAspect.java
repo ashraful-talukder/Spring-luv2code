21ab2a6a6c064fd52218d0d09165d15f4650b9d9
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-05-05 09:58:24 +0600

    after throwing project

---
+++ spring-demo-aop-z-after-throwing/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,80 @@
+package com.luv2code.aopdemo.aspect;
+
+import java.util.List;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Aspect
+@Order(5)
+@Component
+public class MyDemoLoggingAspect {
+	
+	// add a new advice for @AfterReturning on the findAccounts method
+	@AfterReturning(pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+					returning="result")
+	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint,
+													List<Account> result) {
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=========>>> Executing @AfterReturning on method: " + method);
+		
+		// print out the results of method call
+		System.out.println("\n=========>>> result is: " + result);
+		
+		// let's post-process the data .. let's modify it
+		
+		// convert the account name to upper-case
+		convertAccountNamesToUpperCase(result);
+		
+		System.out.println("\n=========>>> result is: " + result);
+	}
+	
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		
+		// loop through accounts
+		for(Account tempAccount: result) {
+			
+			// get the upper-case version of name
+			String theUpperName = tempAccount.getName().toUpperCase();
+			
+			// update the name on the account
+			tempAccount.setName(theUpperName);
+		}
+		
+	}
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAOPExpressions.forDAOPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		System.out.println("===============>>> Executing @Before advice on addAccount()\n");
+		
+		// display the method signature
+		MethodSignature theMethodSignature = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + theMethodSignature);
+		
+		// display the method arguments
+		
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop through the arguments
+		for(Object tempArg: args) {
+			System.out.println(tempArg);
+			
+			if(tempArg instanceof Account) {
+				
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+				
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
+	}
+}
---
+++ spring-demo-aop-z-after-throwing/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,66 @@
+package com.luv2code.aopdemo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import com.luv2code.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+	
+	// add a new method: findAccounts()
+	
+	public List<Account> findAccounts(){
+		
+		List<Account> myAccount = new ArrayList<>();
+		
+		// create sample accounts
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Silver");
+		Account temp3 = new Account("Luca", "Platinum");
+		Account temp4 = new Account("Sena", "Bronze");
+		
+		// add them to our accounts list
+		myAccount.add(temp1);
+		myAccount.add(temp2);
+		myAccount.add(temp3);
+		myAccount.add(temp4);
+		
+		return myAccount;
+	}
+	
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		
+		System.out.println(getClass() + ": Doing my DB work: Adding an account");
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork()");
+		return true;
+	}
+}
