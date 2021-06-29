3d6f57b74a0aea6c3265719f91181e69cfddce94
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-06-29 11:31:26 +0600

    spring basic security on login

---
+++ spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// add our users for in memory authentication
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication().withUser(users.username("Ashraful").password("Ash123").roles("Admin"))
+										.withUser(users.username("Bulbul").password("Bul123").roles("Manager"))
+										.withUser(users.username("Emon").password("Emo123").roles("Employee"));
+	}
+
+	
+	
+}
