8b951e4bcc3bd6976bd9d8635c20cfa6b90434b2
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-07-08 15:18:02 +0600

    @PostConstruct

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,25 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	// define end-point for "/students" -return list of students
-	@GetMapping("/students")
-	public List<Student> getStudents(){
-		List<Student> theStudents = new ArrayList<>();
+	private List<Student> theStudents;
+	
+	// define @PostConstruct to load the student data .. only once!
+	@PostConstruct
+	public void loadData() {
+		
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Ashraful", "Talukder"));
 		theStudents.add(new Student("Ashraful", "Alam"));
 		theStudents.add(new Student("Chayan", "Mistry"));
 		theStudents.add(new Student("Litan", "Sarkar"));
+	}
+	
+	// define end-point for "/students" -return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		
+		
 		
 		return theStudents;
 	}
