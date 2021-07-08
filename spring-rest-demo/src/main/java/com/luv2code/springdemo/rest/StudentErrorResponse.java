150ff864a09534c73b7ed111c87e5e663d1bbd83
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-07-08 16:47:12 +0600

    Exception Handling

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentErrorResponse.java
@@ -0,0 +1,52 @@
+package com.luv2code.springdemo.rest;
+
+public class StudentErrorResponse {
+
+	private int status;
+	private String message;
+	private long timestamp;
+	
+	
+	public StudentErrorResponse() {
+		
+	}
+
+
+	public StudentErrorResponse(int status, String message, long timestamp) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.timestamp = timestamp;
+	}
+
+
+	public int getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+}
--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +46,28 @@ public class StudentRestController {
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
 		
+		// check the studentId against list size
+		if((studentId >= theStudents.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		
 		// just index into the list ... keep it simple for now
 		return theStudents.get(studentId);
+	}
+	
+	// Add an exception handler using @ExceptionHandler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse>handleException(StudentNotFoundException exc){
+		
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimestamp(System.currentTimeMillis());
+		
+		// return ResponseEntity
+		
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 }
