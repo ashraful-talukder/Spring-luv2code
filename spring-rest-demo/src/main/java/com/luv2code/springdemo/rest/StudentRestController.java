5c6a8f27e0be60052da619ddaf102e80460b4928
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-07-08 15:24:39 +0600

    refactoring

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +37,13 @@ public class StudentRestController {
 		
 		
 		return theStudents;
+	}
+	
+	// define end-point for "/students/{studentId}" - return student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		// just index into the list ... keep it simple for now
+		return theStudents.get(studentId);
 	}
 }
