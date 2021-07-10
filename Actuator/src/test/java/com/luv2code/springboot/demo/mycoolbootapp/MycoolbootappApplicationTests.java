f18ea3988f92346e7310fc68d455bba9042e3016
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-07-10 17:36:41 +0600

    actuactor project

---
+++ Actuator/src/test/java/com/luv2code/springboot/demo/mycoolbootapp/MycoolbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
