4d4adb34ed916c60b525c3796abb0a49b5c95504
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-07-12 09:44:08 +0600

    JPA in real work

--- 23-spring-data-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 23-spring-data-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,7 +2,6 @@ package com.luv2code.springboot.cruddemo.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
