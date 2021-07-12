08f5c5b75e8039f90eba2627f0e217bc2eff5690
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-07-12 17:52:58 +0600

    CRUD operation using Thymeleaf

---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/DAO/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.thymeleafdemo.DAO;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it...no need to write any code
+	
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+}
